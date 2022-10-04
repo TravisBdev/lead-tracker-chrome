a4a42560b2132f3d54d18345aa9ca120d2af5864
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-10-04 11:19:12 -0400

    just knocking off some rust

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+let myLeads = [];
+const inputEl = document.querySelector("#input-el");
 const saveBtn = document.querySelector("#input-btn");
+const leadsList = document.querySelector("#leads-list");
 
 saveBtn.addEventListener("click", saveLead);
 
 function saveLead() {
-  console.log("button clicked");
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  for (const lead of myLeads) {
+    let newLead = `<li>${lead}</li>`;
+    leadsList.innerHTML += newLead;
+  }
 }
